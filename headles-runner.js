87b4a8dfa6d642effb2c30ddebe50b93cdd3da0d
oren <oren@optibus.com>
2021-02-05 00:38:40 +0200

    final fix

--- headles-runner.js
+++ headles-runner.js
@@ -1,25 +1,26 @@
 const { runner } = require("mocha-headless-chrome");
-const fs = require("fs");
+// const fs = require("fs");
 const server = require("./test/js/server");
 
 const options = {
   file: "./test/index.html", // test page path
-  reporter: "junit", // mocha reporter name
+  reporter: process.env.REPORT ? "xunit" : null, // mocha reporter name
   width: 800, // viewport width
   height: 600, // viewport height
   timeout: 120000, // timeout in ms
   polling: "raf", // polling mechanism
   // executablePath: '/usr/bin/chrome-unstable',  // chrome executable path
   // visible: true,                               // show chrome window
-  args: ["no-sandbox", "--out report"], // chrome arguments
+  args: ["no-sandbox"], // chrome arguments
 };
 
 async function run() {
   await server.start();
   const result = await runner(options);
-  const str = JSON.stringify(result);
-  fs.writeFileSync("./report.json", str);
+  const str = JSON.stringify(result.result);
+  // fs.writeFileSync("./report.json", str);
   process.exit(0);
 }
 
 run();
+// report();
--- test/js/server-tests.js
+++ test/js/server-tests.js
@@ -7,7 +7,7 @@
 import { assert } from "chai";
 import _ from "lodash";
 import modelData from "./mock-model/cson";
-import SDB from "../../lib/ember-share.js";
+import SDB from "../../lib/ember-share";
 import sdbModel from "./mock-model/sdb-model";
 
 const test = () => {
@@ -15,8 +15,6 @@ const test = () => {
     let App = null;
     let ShareStore = null;
     let schedule = null;
-
-    const toJson = (obj) => JSON.parse(JSON.stringify(obj));
 
     const postJson = function (url, data, delay) {
       let ajaxCall;
@@ -34,22 +32,19 @@ const test = () => {
       ));
     };
 
-    const createDoc = function (cb) {
+    const createDoc = function () {
       const json = _.assign({}, modelData(), {
         id: `test-${new Date().getTime()}`,
       });
-      return ShareStore.createRecord("schedules", json)
-        .then(function (scheduleCreated) {
-          schedule = scheduleCreated;
-          return cb();
-        })
-        .catch(cb);
+      return ShareStore.createRecord("schedules", json).then(function (
+        scheduleCreated
+      ) {
+        schedule = scheduleCreated;
+      });
     };
 
-    const deleteDoc = (cb) =>
-      ShareStore.deleteRecord("schedules", Ember.get(schedule, "id"))
-        .then(cb)
-        .catch(cb);
+    const deleteDoc = () =>
+      ShareStore.deleteRecord("schedules", Ember.get(schedule, "id"));
 
     before(function (done) {
       this.timeout(5000);
@@ -64,18 +59,18 @@ const test = () => {
           });
           app.register("ShareStore:main", SDB.Store);
           app.inject("controller", "ShareStore", "ShareStore:main");
-          return app.inject("route", "ShareStore", "ShareStore:main");
+          app.inject("route", "ShareStore", "ShareStore:main");
         },
       });
 
       App = Ember.Application.create();
       App.Schedule = sdbModel;
-      return (App.ApplicationController = Ember.Controller.extend({
+      App.ApplicationController = Ember.Controller.extend({
         initShareStore: function () {
           ({ ShareStore } = this);
           return this.ShareStore.checkConnection().then(done).catch(done);
         }.on("init"),
-      }));
+      });
     });
 
     beforeEach(createDoc);
@@ -88,7 +83,7 @@ const test = () => {
       op,
     });
 
-    it("set", function (done) {
+    it("set", function () {
       const Obj = Ember.Object.extend({
         schedule,
         cost: Ember.computed.oneWay("schedule.duties.a.stats.cost"),
@@ -98,23 +93,19 @@ const test = () => {
       const cost = obj.get("cost");
 
       const dutyA = schedule.get("duties.a");
-      const statsA = dutyA.get("stats");
 
       assert.equal(cost, 2234);
 
       const op = { p: ["duties", "a", "stats", "cost"], oi: 666, od: 2234 };
 
-      return postJson("op/", createDataOp(op), 0)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          const newCost = obj.get("cost");
-          assert.equal(newCost, 666);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 0).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        const newCost = obj.get("cost");
+        assert.equal(newCost, 666);
+      });
     });
 
-    it("Proxy a new duty", function (done) {
+    it("Proxy a new duty", function () {
       const oldDuty = schedule.get("duties.a").toJson();
 
       const newDuty = {
@@ -131,17 +122,14 @@ const test = () => {
 
       const op = { p: ["duties", "a"], oi: newDuty, od: oldDuty };
 
-      return postJson("op/", createDataOp(op), 0)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          const changedDuty = proxiedDuty.get("content").toJson();
-          assert.deepEqual(newDuty, changedDuty);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 0).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        const changedDuty = proxiedDuty.get("content").toJson();
+        assert.deepEqual(newDuty, changedDuty);
+      });
     });
 
-    it("Proxy inner properties - array", function (done) {
+    it("Proxy inner properties - array", function () {
       const secondEvent = {
         type: "pull in",
         startTime: "11:00",
@@ -163,32 +151,25 @@ const test = () => {
         ld: secondEvent,
       };
 
-      return postJson("op/", createDataOp(op), 100)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          const eventLengthAfter = proxiedDuty.get("events.length");
-          assert.notEqual(eventLengthBefore, eventLengthAfter);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 100).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        const eventLengthAfter = proxiedDuty.get("events.length");
+        assert.notEqual(eventLengthBefore, eventLengthAfter);
+      });
     });
 
-    it("two arrays", function (done) {
-      const order = schedule.get("order");
+    it("two arrays", function () {
       const log = schedule.get("log");
 
       const op = { p: ["log", 0], li: 0 };
 
-      return postJson("op/", createDataOp(op), 0)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          assert.equal(4, log.get("content.length"));
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 0).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        assert.equal(4, log.get("content.length"));
+      });
     });
 
-    it("many logs", function (done) {
+    it("many logs", function () {
       this.timeout(5000);
       const logNumber = [
         0,
@@ -259,12 +240,10 @@ const test = () => {
           assert.isTrue(_.every(msgs, (msgObj) => msgObj.msg === "Success"));
           endLog = obj.get("_log");
           assert.notDeepEqual(middleLog, endLog);
-          return done();
-        })
-        .catch(done);
+        });
     });
 
-    it("get inner id", function (done) {
+    it("get inner id", function () {
       const Vord = Ember.Object.extend({
         id: function () {
           return this.get("b.id");
@@ -283,18 +262,15 @@ const test = () => {
         oi: "e",
       };
 
-      return postJson("op/", createDataOp(op), 0)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          const idAfter = proxiedDuty.get("id");
-          assert.notEqual(idAfter, idBefore);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 0).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        const idAfter = proxiedDuty.get("id");
+        assert.notEqual(idAfter, idBefore);
+      });
     });
 
-    it("Proxy inner properties - array - object", function (done) {
+    it("Proxy inner properties - array - object", function () {
       const serviceEvent = {
         startTime: 1,
         endTime: 2,
@@ -315,16 +291,13 @@ const test = () => {
         od: serviceEvent,
       };
 
-      return postJson("op/", createDataOp(op), 0)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          assert.deepEqual(innerEvent.toJson(), { index: 1 });
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 0).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        assert.deepEqual(innerEvent.toJson(), { index: 1 });
+      });
     });
 
-    it("Proxy replace array 1", function (done) {
+    it("Proxy replace array 1", function () {
       this.timeout(5000);
       const startScheduleEvents = [
         {
@@ -358,18 +331,15 @@ const test = () => {
       const obj = Obj.create({ duty: schedule.get("duties.b") });
       obj.get("events");
 
-      return postJson("op/", createDataOp(op), 200)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          const a = obj.get("events").toArray();
-          assert.deepEqual(endScheduleEvents, a);
-          assert.equal(eventsWasCalledCounter, 2);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 200).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        const a = obj.get("events").toArray();
+        assert.deepEqual(endScheduleEvents, a);
+        assert.equal(eventsWasCalledCounter, 2);
+      });
     });
 
-    it("Add property", function (done) {
+    it("Add property", function () {
       const newDuty = {
         id: "f",
         stats: "s",
@@ -379,33 +349,27 @@ const test = () => {
 
       const duties = schedule.get("duties");
 
-      return postJson("op/", createDataOp(op), 100)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          const dutyF = duties.get("f");
-          assert.deepEqual(dutyF.toJson(), newDuty);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 100).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        const dutyF = duties.get("f");
+        assert.deepEqual(dutyF.toJson(), newDuty);
+      });
     });
 
-    it("Remove property", function (done) {
+    it("Remove property", function () {
       const dutyA = schedule.get("duties.a");
 
       const op = { p: ["duties", "a"], od: dutyA.toJson() };
 
       const duties = schedule.get("duties");
 
-      return postJson("op/", createDataOp(op), 100)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          assert.isUndefined(duties.get("a"));
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 100).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        assert.isUndefined(duties.get("a"));
+      });
     });
 
-    it("Child Limiations (Object)", function (done) {
+    it("Child Limiations (Object)", function () {
       const Obj = Ember.Object.extend({
         limitedObject: function () {
           console.log("should happen twice");
@@ -418,16 +382,13 @@ const test = () => {
 
       const op = { p: ["limitedObject", "some", "data"], oi: 2, od: 1 };
 
-      return postJson("op/", createDataOp(op), 100)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          assert.equal(obj.get("limitedObject"), 2);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 100).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        assert.equal(obj.get("limitedObject"), 2);
+      });
     });
 
-    it("Child Limiations (unscheduled)", function (done) {
+    it("Child Limiations (unscheduled)", function () {
       this.timeout(5000);
 
       const Obj = Ember.Object.extend({
@@ -442,28 +403,22 @@ const test = () => {
 
       const op = { p: ["duties", "b", "unscheduled"], oi: true };
 
-      return postJson("op/", createDataOp(op), 100)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          assert.equal(obj.get("scheduled"), "true");
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 100).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        assert.equal(obj.get("scheduled"), "true");
+      });
     });
 
-    it.skip("op came from an a property that is is not an attribute", function (done) {
+    it.skip("op came from an a property that is is not an attribute", function () {
       const op = { p: ["arr", 1], li: 2 };
       //      op = p: [ 'str' ], oi: 'is', od: "as"
-      return postJson("op/", createDataOp(op), 100)
-        .then(function (response) {
-          assert.equal(response != null ? response.msg : undefined, "Success");
-          console.log(schedule);
-          return done();
-        })
-        .catch(done);
+      return postJson("op/", createDataOp(op), 100).then(function (response) {
+        assert.equal(response != null ? response.msg : undefined, "Success");
+        console.log(schedule);
+      });
     });
 
-    it("test nestedArray replace", function (done) {
+    it("test nestedArray replace", function () {
       let newValue;
       this.timeout(4000);
       const nestedArray = schedule.get("nestedArray");
@@ -500,9 +455,7 @@ const test = () => {
         .then(function (response) {
           assert.equal(response != null ? response.msg : undefined, "Success");
           assert.equal(nestedArray.get("arr").toJson()[4], "p");
-          return done();
-        })
-        .catch(done);
+        });
     });
 
     it("event stand by", () => {
@@ -525,7 +478,7 @@ const test = () => {
 };
 
 export default test;
-// it 'Child Limiations (Array)', (done) ->
+// it 'Child Limiations (Array)', () ->
 //   Obj = Ember.Object.extend
 //     allowDeadHeads: (->
 //       console.log 'get perform'
